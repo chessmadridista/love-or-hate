8099467c84bc694bf187359d250c68e32f292fd3
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-25 02:19:59 +0530

    feat: logout on button click.

--- src/components/app/AppButtonLogout.vue
+++ src/components/app/AppButtonLogout.vue
@@ -1,29 +1,15 @@
 <script setup>
 import { useGeneralStore } from '@/stores';
-import { inject } from 'vue';
 import { useRouter } from 'vue-router'
 
-const axios = inject('axios')
 const generalStore = useGeneralStore()
 const router = useRouter()
 
 function logout() {
-    const endPoint = '/logout'
-    axios.post(endPoint)
-        .then((response) => {
-            console.log(response)
-            generalStore.setSnackbarMessage(response.data.message)
-            generalStore.setSnackbarColor('success')
-            router.push('login')
-        })
-        .catch((error) => {
-            console.error(error)
-            generalStore.setSnackbarMessage(error.response.data.message)
-            generalStore.setSnackbarColor('error')
-        })
-        .finally(() => {
-            generalStore.showSnackbar()
-        })
+    router.push('login')
+    generalStore.setSnackbarMessage("You have logged out successfully.")
+    generalStore.setSnackbarColor('success')
+    generalStore.showSnackbar()
 }
 </script>
 <template>
