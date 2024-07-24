4c5e72c55c2029d81d78a15d070f624b109ab6a0
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-25 02:17:21 +0530

    feat: improve the position of the username and its dp.

--- src/components/home/HomePosts.vue
+++ src/components/home/HomePosts.vue
@@ -8,14 +8,22 @@ const postStore = usePostStore()
         <v-row v-for="post in postStore.posts" :key="post.id">
             <v-col>
                 <v-card class="pa-4">
-                    <v-avatar :image="post.usernameDpSrc" size="small" /><span class="ml-2">{{ post.username }}</span>
-                    <v-card-title>{{ post.title }}</v-card-title>
+                    <v-container>
+                        <v-avatar :image="post.usernameDpSrc" size="small" /><span class="ml-2">{{ post.username }}</span>
+                    </v-container>
                     <v-card-subtitle>
-                        {{ post.dateCreated }}
+                        {{ post.dateCreated }} | <v-icon color="green">mdi-heart</v-icon>:<v-icon color="red">mdi-heart-broken</v-icon> = {{ post.loveToHateRatio }}
                     </v-card-subtitle>
+                    <v-card-title>{{ post.title }}</v-card-title>
                     <v-card-text>
                         {{ post.description }}
                     </v-card-text>
+                    <v-card-actions>
+                        <v-icon color="green">mdi-heart</v-icon>{{ post.noOfLoves }}
+                        <v-icon color="red" class="ml-2">mdi-heart-broken</v-icon>{{ post.noOfHates }}
+                        <v-icon color="yellow" class="ml-2">mdi-comment-outline</v-icon>{{ post.noOfComments }}
+                        <v-icon color="blue" class="ml-2">mdi-share</v-icon>{{ post.noOfHates }}
+                    </v-card-actions>
                 </v-card>
             </v-col>
         </v-row>
