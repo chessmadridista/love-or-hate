c439ff83647c1c1ac88e38473125d19a2fffab77
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-29 19:53:49 +0530

    feat: add card title.

--- src/components/user/UserBio.vue
+++ src/components/user/UserBio.vue
@@ -1,12 +1,16 @@
 <script setup>
+import {useRouter} from 'vue-router'
+
+const router = useRouter()
+const username = router.currentRoute.value.params.username
 </script>
 <template>
     <v-container>
         <v-row>
             <v-col>
                 <v-card>
-                    <v-card-title>
-                        @{{  }}
+                    <v-card-title class="text-primary font-weight-bold">
+                        @{{ username }}
                     </v-card-title>
                 </v-card>
             </v-col>
