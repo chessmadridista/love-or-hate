7dc573d9cb37c23f53f5007adb9d6b3c699bd413
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-29 20:28:33 +0530

    feat: add share functionality.

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import UserBio from '@/components/user/UserBio.vue'
 import UserTabsPostsAndComments from '@/components/user/UserTabsPostsAndComments.vue'
+import AppPostShareDialog from '@/components/app/AppPostShareDialog.vue';
 </script>
 <template>
     <UserBio />
     <UserTabsPostsAndComments />
+    <AppPostShareDialog />
 </template>
