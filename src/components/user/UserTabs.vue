381d224ac09b020a19c211b189bbcbe2d29dbbf4
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-29 20:11:44 +0530

    feat: move tabs to a separate section.

---
+++ src/components/user/UserTabs.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue'
+
+const tab = ref('posts')
+</script>
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-tabs color="primary" v-model="tab" fixed-tabs>
+                        <v-tab>Posts</v-tab>
+                        <v-tab>Comments</v-tab>
+                    </v-tabs>
+                </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-tabs-window v-model="tab" fixed-tabs>
+                    <v-tabs-window-item value="posts">
+                        <v-card>
+                            <v-card-text>
+                                This is a post.
+                            </v-card-text>
+                        </v-card>
+                    </v-tabs-window-item>
+                    <v-tabs-window-item value="comments">
+                        <v-card>
+                            <v-card-text>
+                                This is a comment.
+                            </v-card-text>
+                        </v-card>                        
+                    </v-tabs-window-item>
+                </v-tabs-window>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import UserBio from '@/components/user/UserBio.vue'
+import UserTabs from '@/components/user/UserTabs.vue'
 </script>
 <template>
     <UserBio />
+    <UserTabs />
 </template>
