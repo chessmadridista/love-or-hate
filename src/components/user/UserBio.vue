7028b6fea5db3351b7ab0c9174028ef46977a836
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-07-29 19:55:05 +0530

    feat: add user bio.

--- src/components/user/UserBio.vue
+++ src/components/user/UserBio.vue
@@ -3,6 +3,7 @@ import {useRouter} from 'vue-router'
 
 const router = useRouter()
 const username = router.currentRoute.value.params.username
+const bio = 'Looking to make this world a better place when I leave this world.'
 </script>
 <template>
     <v-container>
@@ -12,6 +13,9 @@ const username = router.currentRoute.value.params.username
                     <v-card-title class="text-primary font-weight-bold">
                         @{{ username }}
                     </v-card-title>
+                    <v-card-text>
+                        {{ bio }}
+                    </v-card-text>
                 </v-card>
             </v-col>
         </v-row>
